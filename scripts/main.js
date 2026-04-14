5f08b95fdc477a6206b240beb109c0e2acda1783
Cherlia Fleming <cherlia91@gmail.com>
2026-04-14 18:57:31 -0500

    updated main.sfile

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import { parkAreas } from './parkAreas.js'
+import { guestArea } from './guests.js'
+import { serviceAreas } from './services.js'
+
+document.addEventListener('DOMContentLoaded' , () => {
+    parkAreas()
+    guestArea()
+    serviceAreas()
+})
