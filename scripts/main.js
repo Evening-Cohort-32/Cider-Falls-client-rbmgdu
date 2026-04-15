7794098d79717aac80a5c29e71301bd0bcaec582
Lake Carter <lakecarter8@gmail.com>
2026-04-14 20:56:38 -0500

    created fuction for the parkArea HTML and created the main site's HTML template in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,26 @@ document.addEventListener('DOMContentLoaded' , () => {
     guestArea()
     serviceAreas()
 })
+const mainContainer = document.querySelector("#container");
+
+//The main HTML for the site is created
+const mainHTML = `
+    <header>
+        <img id = logo src = https://www.nps.gov/theme/assets/dist/images/branding/logo.png>
+        <h1>Cider Falls Park</h1>
+    </header>
+        <article class = details>
+            <section class = parks>
+                ${parkAreas()}
+            </section>
+            <section class = guests>
+                <H2>Guests</H2>
+                ${guestArea()}
+            </section>
+        </article>
+    <footer>
+        <ul>555-555-555</ul>
+        <ul>Ciderfallspark.org</ul>
+        <ul>123 Forrest Road, Chatt Falls, TN</ul>
+    </footer>
+`
---
+++ scripts/parkAreas.js
@@ -0,0 +1,21 @@
+import {getParkAreas, getServices} from "./database.js"
+
+const allAreas = getParkAreas()
+
+let parkAreaHTML = ``
+
+export const parkAreas = () => {
+    for(const area of allAreas){
+        parkAreaHTML += `
+        <section id = "parkAreaDetails" 
+        data.type = "parkArea"
+        data.area_id = "${area.area_id}"
+        data.area_name = "${area.area_name}"
+        data.location = "${area.location}"
+        >
+            <h2>${area.area_name}</h2>
+            ${services(area)}
+        </section>
+        `
+    }
+}
