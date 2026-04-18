4baca6ea35d39042f3b840b450b7df52ab59f9d2
Cherlia Fleming <cherlia91@gmail.com>
2026-04-18 10:04:26 -0500

    added findServices file to list the services and their location and updated main.js clickEvent

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import { createParkAreas } from './parkAreas.js'
 import { createGuests } from './guests.js'
+import { createServiceList } from './createServices.js'
+import { services, parkAreas, areaServices } from './database.js'
+
 
 // document.addEventListener('DOMContentLoaded' , () => {
 //     parkAreas()
@@ -15,7 +18,11 @@ const mainHTML = `
         <img id = logo src = https://www.nps.gov/theme/assets/dist/images/branding/logo.png>
         <h1>Cider Falls Park</h1>
     </header>
-        <article class = details>
+        <article class=details>
+        <section class=services>
+            ${createServiceList()}
+            </section>
+              <div id="service-message"></div>
             <section class = parks>
                 ${createParkAreas()}
             </section>
@@ -31,3 +38,29 @@ const mainHTML = `
     </footer>
 `
 mainContainer.innerHTML = mainHTML
+document.addEventListener("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+
+    if (itemClicked.dataset.type === "service") {
+        const serviceId = parseInt(itemClicked.dataset.id)
+
+        const matchingAreaServices = areaServices.filter(
+            (as) => as.serviceId === serviceId
+        )
+
+        const supportingAreas = []
+        for (const match of matchingAreaServices) {
+            const foundArea = parkAreas.find(
+                (area) => area.id === match.parkAreaId
+            )
+            supportingAreas.push(foundArea.name)
+        }
+
+        const clickedService = services.find((s) => s.id === serviceId)
+
+        document.querySelector("#service-message").innerHTML = `
+            <p>${clickedService.name} is supported by: 
+            ${supportingAreas.join(", ")}</p>
+        `
+    }
+})
--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -9,10 +9,10 @@ export const createParkAreas = () => {
     for(const area of allAreas){
         parkAreaHTML += `
         <section id = "parkAreaDetails" 
-        data.type = "parkArea"
-        data.area_id = "${area.id}"
-        data.area_name = "${area.name}"
-        data.location = "${area.location}"
+        data-type = "parkArea"
+        data-area_id = "${area.id}"
+        data-area_name = "${area.name}"
+        data-location = "${area.location}"
         >
             <h3>${area.location}</h3>
             <h2>${area.name}</h2>
